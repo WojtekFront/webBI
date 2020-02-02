43e9433192a14a695676d9fac74b2ca5e0bfe7a7
„wojtek <wojciech_lisowski@wp.pl>
2020-02-02 22:25:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Maven+Pro:400,500,700,900&display=swap&subset=latin-ext" rel="stylesheet">
+    <link href="style/style.css" type="text/css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container"><!--open container  -->
+<div class="head"><!--open head-->
+
+<div class="topA navbar navbar-expand-lg"><!--open topA bootstrap navbar-->
+
+  
+ 
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+    <div class="navbar-nav ">
+      <a class="nav-item nav-link" href="#">PORTFOLIO</a>
+      <a class="nav-item nav-link" href="#">ABOUT</a>
+      <a class="nav-item nav-link" href="#"style="width:100px"></a>
+      <a class="nav-item nav-link" href="#">BLOG</a>
+      <a class="nav-item nav-link" href="#">GET IN TOUCH</a>
+    </div>
+  </div>
+
+
+
+
+<!-- wprowadzenie bootsrap-->
+    
+
+</div><!--close topA-->
+<div class="topB"><!-- open top -->
+<div class="triangle"><!--open triangle-->
+</div><!--close triangle-->
+</div><!--close top-->
+
+</div><!--close head-->
+
+
+
+
+
+<div class="content"> <!--open content-->
+
+
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+ 
+<ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item active">
+      <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
+    </div>
+
+
+  </div>
+</div>
+
+ 
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<!-- <img src="img/pic1.jpg" alt="picture"> -->
+</div><!--close content-->
+<div class="aboutMe"><!--open aboutMe-->
+<p>Tekst tekst</p>
+</div><!--close aboutMe-->
+
+<div class="getInTouch"><!--open getInTouch-->
+
+
+
+
+</div><!--close getInTouch-->
+    </div><!--close container-->
+
+    <footer>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
