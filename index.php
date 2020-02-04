34a430a7dd1adb0375c36fe8f163b68174aca9f9
„wojtek <wojciech_lisowski@wp.pl>
2020-02-04 15:59:13 +0100

    add about me

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Maven+Pro:400,500,700,900&display=swap&subset=latin-ext" rel="stylesheet">
-    <link href="style/style.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Maven+Pro:400,500,600,900&display=swap&subset=latin-ext" rel="stylesheet">    
+    <link href="style/styleCssCopy.css" type="text/css" rel="stylesheet">
 
     <title>Document</title>
 </head>
@@ -17,24 +17,16 @@
 <div class="head"><!--open head-->
 
 <div class="topA navbar navbar-expand-lg"><!--open topA bootstrap navbar-->
-
-  
  
   <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-    <div class="navbar-nav ">
-      <a class="nav-item nav-link" href="#">PORTFOLIO</a>
-      <a class="nav-item nav-link" href="#">ABOUT</a>
-      <a class="nav-item nav-link" href="#"style="width:100px"></a>
-      <a class="nav-item nav-link" href="#">BLOG</a>
-      <a class="nav-item nav-link" href="#">GET IN TOUCH</a>
+    <div class="navbar-nav">
+      <a class="nav-item nav-link nstyle" href="#">PORTFOLIO</a>
+      <a class="nav-item nav-link nstyle" href="#">ABOUT</a>
+      <a class="nav-item nav-link nstyle" href="#"style="width:100px"></a>
+      <a class="nav-item nav-link nstyle" href="#">BLOG</a>
+      <a class="nav-item nav-link nstyle" href="#">GET IN TOUCH</a>
     </div>
   </div>
-
-
-
-
-<!-- wprowadzenie bootsrap-->
-    
 
 </div><!--close topA-->
 <div class="topB"><!-- open top -->
@@ -44,14 +36,7 @@
 
 </div><!--close head-->
 
-
-
-
-
 <div class="content"> <!--open content-->
-
-
-
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
  
@@ -66,14 +51,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
+      <img src="img/pic2.jpg"  class="d-block w-100" alt=".">
     </div>
-
     <div class="carousel-item">
       <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
+      <img src="img/pic4.jpg"  class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item active">
       <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
@@ -88,24 +72,21 @@
 
   </div>
 </div>
-
- 
-
-</div>
-
-
-
-
-
-
-
-
-
-
-<!-- <img src="img/pic1.jpg" alt="picture"> -->
 </div><!--close content-->
+
+
+
 <div class="aboutMe"><!--open aboutMe-->
-<p>Tekst tekst</p>
+<div class="aboutMeContent"><!--open aboutMeContent-->
+<div class="aboutMePicture">
+  <img src="img/portAutor.jpg">
+</div>
+<div class="aboutMeText">
+  <h1>Smith Anderson</h1>
+  <h2>Illustrator & Character Designer</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna. Integer gravida, diam congue molestie fringilla, elit odio rhoncus turpis, ac dictum libero elit vel metus. Morbi egestas sit amet velit eget venenatis. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna</p>
+</div>
+</div> <!--close aboutMeContent-->
 </div><!--close aboutMe-->
 
 <div class="getInTouch"><!--open getInTouch-->
@@ -114,10 +95,13 @@
 
 
 </div><!--close getInTouch-->
+
+<footer>
+  <p>© Copyright 2013 by PSDchat</p>
+</footer>
     </div><!--close container-->
 
-    <footer>
-</footer>
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
