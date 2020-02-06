f43445941c1f6f8e55d0d628b9d0b1fea398e205
„wojtek <wojciech_lisowski@wp.pl>
2020-02-06 11:46:56 +0100

    more bootstrap

--- index.php
+++ index.php
@@ -2,207 +2,174 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Maven+Pro:400,500,600,900&display=swap&subset=latin-ext" rel="stylesheet">    
-    <link href="style/style.css" type="text/css" rel="stylesheet">
-
-    <title>Document</title>
-</head>
-<body>
    
-<nav class="navbar navbar-expand-lg  navbar-expand-md navbar-dark bg-dark"><!--open topA bootstrap navbar-->
-<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-  <div class="collapse navbar-collapse justify-content-center " id="navbarMenu" id="navbarNavAltMarkup">
-       
-      <ul class="navbar-nav">
-     <li class="nav-item">
-      <a class="nav-item nav-link nstyle" href="#">PORTFOLIO</a>
-</li><li class="nav-item">
-      <a class="nav-item nav-link nstyle" href="#">ABOUT</a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-item nav-link nstyle" href="#"style="width:100px"></a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-item nav-link nstyle" href="#">BLOG</a>
-      </li>
-      <li class="nav-item"> 
-      <a class="nav-item nav-link nstyle" href="#">GET IN TOUCH</a>
-      </li>
-    </ul>
+    <title>Document1</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
     
-    <img src="img/logo.png" class="logo" alt="...">
-</div>
+     <link href="style/styleCsscopy.css" type="text/css" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Maven+Pro:400,500,600,900&display=swap&subset=latin-ext" rel="stylesheet">     
 
-
-</nav><!--close topA--> 
-
-
-<nav class="navbar navbar-expand-lg navbar-expand-md navbar-expand-xl navbar-dark bg-dark  tt"><!--open topA bootstrap navbar-->
-<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-  <div class="collapse navbar-collapse  justify-content-center tt" id="navbarMenu" >
-       
-      <ul class="navbar-nav tt">
-     <li class="nav-item">
-      <a class="nav-link" href="#">PORTFOLIO</a>
-        </li><li class="nav-item">
-      <a class="nav-link" href="#">ABOUT</a>
-        </li>
-      <li class="nav-item">
-      <a class="nav-link" href="#"style="width:100px"><div><img src="img/logo.png" id="logo" alt="..." ></div></a>
-        </li>
-      <li class="nav-item">
-      <a class="nav-link" href="#">BLOG</a>
-      </li>
-      <li class="nav-item"> 
-      <a class="nav-link" href="#">GET IN TOUCH</a>
-      </li>
-    </ul>
     
-    
-</div>
 
-</nav><!--close topA-->
+</head>
 
+<body>
+  
+<div class="container"><!--open container  -->
 
+<div class="row">
+    <nav class="navbar navbar-expand-lg  navbar-expand-md navbar-dark bg-dark col-sm-12"><!--open topA bootstrap navbar-->
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center " id="navbarMenu" id="navbarNavAltMarkup">
+          
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-item nav-link nstyle" href="#">PORTFOLIO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-item nav-link nstyle" href="#">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-item nav-link nstyle" href="#"style="width:100px"><img src="img/logo.png" class="logo" alt="..."></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-item nav-link nstyle" href="#">BLOG</a>
+            </li>
+            <li class="nav-item"> 
+              <a class="nav-item nav-link nstyle" href="#">GET IN TOUCH</a>
+            </li>
+          </ul>
+        
+        
+      </div>
+  </nav><!--close topA--> 
+  </div><!-- close row -->
+      
+  <div class="col-sm-12 head"><!--open head-->
 
-    <div class="container"><!--open container  -->
-<div class="head"><!--open head-->
+  <div class=" triangle"><!--open triangle-->
+  </div><!--close triangle-->
 
+  </div><!--close head-->
 
+  <div class="content"> <!--open content-->
 
-
-
-<div class="topB"><!-- open top -->
-<div class="triangle"><!--open triangle-->
-</div><!--close triangle-->
-</div><!--close top-->
-
-</div><!--close head-->
-
-<div class="content"> <!--open content-->
-
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
- 
-        <ol class="carousel-indicators">
-         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-        </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/pic2.jpg"  class="d-block w-100" alt=".">
-    </div>
-    <div class="carousel-item">
-      <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/pic4.jpg"  class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item active">
-      <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
-    </div>
-
-
-  </div><!-- close carousel-inner -->
-</div><!-- close carouselExampleIndicators -->
-</div><!-- close content -->
-
-
-
-<div class="aboutMe"><!--open aboutMe-->
-  <div class="aboutMeContent"><!--open aboutMeContent-->
-
-    <div class="aboutMePicture">
-      <img src="img/portAutor.jpg">
-    </div><!-- close aboutMePicture -->
-
-    <div class="aboutMeText">
-      <h1>Smith Anderson</h1>
-      <h2>Illustrator & Character Designer</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna. Integer gravida, diam congue molestie fringilla, elit odio rhoncus turpis, ac dictum libero elit vel metus. Morbi egestas sit amet velit eget venenatis. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna</p>
-    </div><!--aboutMeText-->
-
-  </div><!--close aboutMeContent-->
-</div><!--close aboutMe-->
-
-
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  
+          <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+          </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/pic2.jpg"  class="d-block w-100" alt=".">
+        </div>
+        <div class="carousel-item">
+          <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/pic4.jpg"  class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item active">
+          <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/pic1.jpg"  class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/pic2.jpg"  class="d-block w-100" alt="...">
+        </div>
 
 
+      </div><!-- close carousel-inner -->
+  </div><!-- close carouselExampleIndicators -->
+  </div><!-- close content -->
 
 
-<div class="getInTouch"><!--open getInTouch-->
-    <div class="formDe">
-        <h3>Get In Touch</h3>
-        <h4>In order to get in touch use the contact form below:</h4>
-    </div><!--close formDe-->
-<div class="form">
 
-<form>
-  <div class="form-group">
-    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name (Required)" required>
-  </div>
-  <div class="form-group">
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email (Required)" required>
-  </div>
-  <div class="form-group">
-    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Subject (Required)" >
-  </div>
+  <div class="aboutMe"><!--open aboutMe-->
+    <div class="aboutMeContent"><!--open aboutMeContent-->
 
-  <div class="form-group">
-    <textarea class="form-control" id="descText" rows="8" placeholder="Write your message here..." required></textarea >
-  </div>
-  <div class="form-group">
-  <button type="submit" class="btn btn-primary">SEND</button>
-  </div>
-</form>
-</div><!--close form-->
-<div class="contact">
-<div class="infoContact">
-    <p id="LB">Quisque Hendrerit:</p><p id="LR"> purus dapibus, ornare nibh vitae, viverra nibh. Fusce vitae aliquam tellus. Proin sit amet volutpat libero. Nulla sed nunc et tortor luctus faucibus morbi vitae. </p>
-</div>
+      <div class=" col-sm-3 aboutMePicture">
+        <img src="img/portAutor.jpg" class="img-fluid">
+      </div><!-- close aboutMePicture -->
 
-<address><!-- open address -->
-Elm St. 14/05 Lost City <br/>
-+ 3528 331 86 35 <br/>
-info@hexalcorp.com
-</address><!--close address-->
+      <div class="aboutMeText col-sm-7">
+        <h1>Smith Anderson</h1>
+        <h2>Illustrator & Character Designer</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna. Integer gravida, diam congue molestie fringilla, elit odio rhoncus turpis, ac dictum libero elit vel metus. Morbi egestas sit amet velit eget venenatis. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna</p>
+      </div><!--aboutMeText-->
 
-</div><!--close contact-->
-</div><!--close getInTouch-->
-
-<footer>
-  <p>© Copyright 2013 by PSDchat</p>
-</footer>
-    </div><!--close container-->
-
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </div><!--close aboutMeContent-->
+  </div><!--close aboutMe-->
 
 
 
 
 
+
+  <div class="getInTouch"><!--open getInTouch-->
+      <div class="formDe">
+          <h3>Get In Touch</h3>
+          <h4>In order to get in touch use the contact form below:</h4>
+      </div><!--close formDe-->
+      <div class="form">
+
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name (Required)" required>
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email (Required)" required>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Subject (Required)" >
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" id="descText" rows="8" placeholder="Write your message here..." required></textarea >
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">SEND</button>
+          </div>
+        </form>
+      </div><!--close form-->
+    <div class="contact">
+      <div class="infoContact">
+        <p id="LB">Quisque Hendrerit:</p><p id="LR"> purus dapibus, ornare nibh vitae, viverra nibh. Fusce vitae aliquam tellus. Proin sit amet volutpat libero. Nulla sed nunc et tortor luctus faucibus morbi vitae. </p>
+      </div>
+
+      <address><!-- open address -->
+        Elm St. 14/05 Lost City <br/>
+        + 3528 331 86 35 <br/>
+        info@hexalcorp.com
+      </address><!--close address-->
+
+    </div><!--close contact-->
+  </div><!--close getInTouch-->
+
+  <footer>
+    <p>© Copyright 2013 by PSDchat</p>
+  </footer>
+      
+
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+
+
+</div><!--close container-->
 </body>
 </html>
