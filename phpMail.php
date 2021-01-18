31197b277034de28dea97a19f4c49c2857d4d3f1
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2021-01-18 20:28:05 +0100

    edit  29 line in phpMail
    cut 
    $mail = new PHPMailer(true);

--- phpMail.php
+++ phpMail.php
@@ -3,6 +3,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,72 +11,75 @@ session_start();
     <title>Document</title>
 
 </head>
+
 <body>
-    
-<?php
 
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    <?php
 
-// Load Composer's autoloader
-require 'vendor/autoload.php';
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-// Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer(true);
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
 
-try {
-    //Server settings
-    $mail->SMTPDebug = 5;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.wp.pl';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'testkodu1@wp.pl';                     // SMTP username
-    $mail->Password   = '123456Ab!';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = '465';                                    // TCP port to connect to
-    $mail->CharSet = "UTF-8";
-    $mail->setLanguage('pl', '/phpmailer/language');
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer;
 
-
-
-
-// name, email, subject, message
-$name=$_POST["name"];
-$email=$_POST["email"];
-$subject=$_POST["subject"];
-$message=$_POST["message"];
+    try {
+        //Server settings
+        $mail->SMTPDebug = 5;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.wp.pl';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = '...........email';                     // SMTP username
+        $mail->Password   = '...........password';                               // SMTP password
+        $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = '465';                                    // TCP port to connect to
+        $mail->CharSet = "UTF-8";
+        $mail->setLanguage('pl', '/phpmailer/language');
 
 
 
 
-    // Attachments
-   // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-   //$mail->addAttachment('CVLisowski.pdf');    // Optional name
-   $mail->setFrom('testkodu1@wp.pl', 'Mailer');
-   $mail->addAddress($email, $name);
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'wiadomosc ze strony Smith Anderson';
-    $mail->Body    = "$subject.<br><br>.$message";
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        // name, email, subject, message
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
 
 
-for($i=0;$i<=0;$i++){
-        $_SESSION['info'] = '<span style="color:red">wiadomość została wysłana!</span>';
-                header('Location: index.php');
-                $mail->send();
-                exit;}
-} catch (Exception $e) {
-    $_SESSION['info'] = '<span style="color:red">Message could not be sent. Mailer Error: {$mail->ErrorInfo};</span>';
-
-    header('Location: index.php');
-    exit;
-}
 
 
-?>
+        // Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('CVLisowski.pdf');    // Optional name
+        $mail->setFrom('wojtan123@wp.pl', 'Mailer');
+        $mail->addAddress($email, $name);
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'wiadomosc ze strony Smith Anderson';
+        $mail->Body    = "$subject.<br><br>.$message";
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+
+        for ($i = 0; $i <= 0; $i++) {
+            $_SESSION['info'] = '<span style="color:red">wiadomość została wysłana!</span>';
+            header('Location: index.php');
+            $mail->send();
+            exit;
+        }
+    } catch (Exception $e) {
+        $_SESSION['info'] = '<span style="color:red">Message could not be sent. Mailer Error: {$mail->ErrorInfo};</span>';
+
+        header('Location: index.php');
+        exit;
+    }
+
+
+    ?>
 </body>
+
 </html>
